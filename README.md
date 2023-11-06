4072299a685699e6fc7f50c4119a81a66b2e82a1
Your Name~ <hmphtetminpaing@gmail.com>
2023-11-06 11:04:34 +0630

    Header component added

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ npm install -D tailwindcss postcss autoprefixer
 
 npx tailwindcss init -p
 
+npm run dev
+
+npm i react-icons
+
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import Signin from './pages/Signin'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/sigin-up' element={<SignUp/>} />
-      <Route path='/sign-in' element={<Signin/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/profile' element={<Profile/>} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sigin-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   )
 }
