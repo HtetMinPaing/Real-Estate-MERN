180098676db9e09ae4387375d600dda1ed74ef6b
Your Name~ <hmphtetminpaing@gmail.com>
2023-11-23 19:50:03 +0630

    Add About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur nisi et dictum lacinia. In eget ultrices velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla ac mollis tortor. Phasellus pharetra, nulla nec ultricies ullamcorper, lorem ante porttitor enim, nec tincidunt mi velit nec orci. Vestibulum ullamcorper sed dolor eget vestibulum. Nunc posuere non nibh eget malesuada. Vestibulum pellentesque mauris ut nunc semper sagittis. Mauris dignissim dolor ac ex convallis pretium. Praesent vitae viverra ligula. Praesent sit amet dui tempus, malesuada nibh sagittis, mollis lorem. Mauris lacus lorem, bibendum vitae quam quis, accumsan luctus justo. Praesent in ex ac ante tempor commodo vitae quis purus. Aliquam quis nulla at lorem pulvinar dignissim id eget quam.</p>
+    </div>
   )
 }
 
